c3e70edba999b86bbde58670a478808f2a69746a
Umair Afridi <umairofficial0f@gmail.com>
2026-06-29 22:45:51 +0500

    Hardcoded database name

--- server.js
+++ server.js
@@ -113,7 +113,7 @@ const db = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    database: 'defaultdb',
     port: process.env.DB_PORT || 27670,
     waitForConnections: true,
     connectionLimit: 10,
